3f538484fe89badae5fa5c3270a57de7de75cb97
GioDefra <giovanni.defrancesco@supermercato24.it>
2018-01-24 19:16:11 +0100

    better design.

--- app/src/main/java/it/duir/timbramitutto/MainActivity.kt
+++ app/src/main/java/it/duir/timbramitutto/MainActivity.kt
@@ -1,14 +1,39 @@
 package it.duir.timbramitutto
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+  private var started = false
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    main_timer.text = "00:00:00"
+    main_timer_btn.setOnClickListener(toggleTimerButton())
+  }
+
+  private fun toggleTimerButton() = { view: View? ->
+    val btn: FloatingActionButton = view as FloatingActionButton
+    //    if (started) {
+    //      main_end_timer.text = System.currentTimeMillis().toString()
+    //    } else {
+    //      main_begin_timer.text = System.currentTimeMillis().toString()
+    //    }
+    started = !started
+    val color = if (started) {
+      btn.setImageResource(R.drawable.ic_stop)
+      ContextCompat.getColor(this, R.color.red_orange)
+    } else {
+      btn.setImageResource(R.drawable.ic_play_arrow)
+      ContextCompat.getColor(this, R.color.emerald)
+    }
+    btn.backgroundTintList = ColorStateList.valueOf(color)
+//    btn.rippleColor = color
   }
 }
